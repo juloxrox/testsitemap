f8234b1d52b04ef8ed0a2cefee8003fa52b60567
Julien CLEREL <jclerel.freelance@gmail.com>
2019-01-29 20:12:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# World-class tech stack for Web entrepreneurs
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ yarn install && cd functions && yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn run dev
+
+# build for production and launch server
+$ yarn run build
+$ yarn start
+
+# firebase
+§ firebase init (only functions)
+$ firebase serve
+
+# deploy
+$ firebase deploy
+```
---
+++ nuxt.config.js
@@ -0,0 +1,78 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+  srcDir: 'src',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+	  '@nuxtjs/sitemap'
+  ],
+
+  sitemap: {
+    path: '/sitemap.xml',
+    hostname: 'https://example2.com',
+    cacheTime: 1,
+    gzip: true,
+    generate: false, // Enable me when using nuxt generate
+    exclude: [
+      '/secret',
+      '/admin/**'
+    ],
+    routes: [
+      '/page/1',
+      {
+        url: '/page/2',
+        changefreq: 'daily',
+        priority: 1,
+        lastmodISO: '2017-06-30T13:30:00.000Z'
+      }
+    ]
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      
+    }
+  }
+}
